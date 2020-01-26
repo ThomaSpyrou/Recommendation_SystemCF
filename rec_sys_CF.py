753a132da723de165606028c88a9eb2abab29c04
ThomaSpyrou <spyroutommy@gmail.com>
2020-01-26 23:30:14 +0200

    Read data from CSV files, processed them as requested, wrote to a new merged CSV file.

--- rec_sys_CF.py
+++ rec_sys_CF.py
@@ -28,6 +28,7 @@ def read_data():
         books = pd.read_csv('BX-CSV-Dump/BX-Books.csv', delimiter=";", error_bad_lines=False, encoding='latin1')
         books.columns = ['ISBN', 'bookTitle', 'bookAuthor', 'yearOfPublication', 'publisher', 'imageURLS', 'imageURLM',
                          'imageURLL']
+
         books.drop(['imageURLS', 'imageURLM', 'imageURLL'], axis=1, inplace=True)
 
         return book_ratings, users, books
@@ -36,30 +37,56 @@ def read_data():
         print("CSV files are not in proper format.")
 
 
+def stem(cell):
+    stemming = PorterStemmer()
+    word_list = cell
+    stemmed_list = stemming.stem(word_list)
+
+    return stemmed_list
+
+
 def pre_processing(book_ratings, users, books):
     """
-    Delete books with less than 10 ratings and  users that have rate less than 5 books.
+    Delete books with less than 10 ratings and  users that have rate less than 5 books. Stem the the keywords
     :param book_ratings: dataframe (pd)
     :param users: dataframe (pd)
     :param books: dataframe (pd)
     :return: processed data
     """
-    counter_of_users = book_ratings['userId'].value_counts()
-    counter_of_ratings = book_ratings['ISBN'].value_counts()
+    count_userId = book_ratings.groupby("userId")["userId"].transform(len)
+    count_ISBN = book_ratings.groupby("ISBN")["ISBN"].transform(len)
 
-    # find books which have less than 10 ratings
-    books_to_delete = []
-    for index, value in counter_of_ratings.items():
-        if int(value) < 10:
-            books_to_delete.append(index)
+    mask_for_book_ratings = (count_userId >= 10) & (count_ISBN >= 5)
+    final_book_ratings = book_ratings[mask_for_book_ratings]
 
-    # find user who have rate less than 5 books
-    users_to_delete = []
-    for index, value in counter_of_users.items():
-        if int(value) < 5:
-            users_to_delete.append(index)
+    new_books = books[(count_ISBN >= 5)]
+    for index, value in new_books.iterrows():
+        value.bookTitle = stem(value.bookTitle)
+
+    new_users = users[(count_userId >= 10)]
+    # user's age goes from nan to 244. Need to fix it. But I have to change and the book ratings **
+    # new_users = new_users[(new_users.age >= 5) | (new_users.age <= 100)]
+
+    return final_book_ratings, new_users, new_books
+
+
+def merge_tables(final_book_ratings, new_users, new_books):
+    """
+    Using panda's merge to create one table.
+    :param final_book_ratings: dataFrame
+    :param new_users: dataFrame
+    :param new_books: dataFrame
+    :return: merged table
+    """
+    rating_book = pd.merge(final_book_ratings, new_books, on='ISBN')
+    all_ratings = pd.merge(rating_book, new_users, on='userId')
+    all_ratings.to_csv('all_ratings_merged', sep='\t', encoding='utf-8', index=False)
 
 
 if __name__ == '__main__':
+    import time
+    start = time.time()
     book_ratings, users, books = read_data()
-    pre_processing(book_ratings, books, users)
+    final_book_ratings, new_users, new_books = pre_processing(book_ratings, users, books)
+    merge_tables(final_book_ratings, new_users, new_books)
+    print(time.time() - start)
